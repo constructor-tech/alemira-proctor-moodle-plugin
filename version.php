93283132e4e67a46a5f3d2f3dc8e107678ed891d
Ilya Volkov <iillyyaa159@gmail.com>
2026-01-20 15:02:25 +0100

    Remove .DS_Store file, update README to reflect Moodle compatibility with versions 3.8 to 5.x, and increment plugin version to 2026012000.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_proctor';
-$plugin->version = 2026011200;
+$plugin->version = 2026012000;
 $plugin->release = 'v2.2';
 $plugin->requires = 2018111800;
 $plugin->maturity = MATURITY_STABLE;
